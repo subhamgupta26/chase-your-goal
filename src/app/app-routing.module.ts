0a1f2028e2061d2623c4ad8ec444593eab97b884
sugupta <sugupta@manh.com>
2019-02-09 16:36:33 +0530

    project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './app/dashboard/dashboard.component';
+import { HomeComponent } from './app/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
